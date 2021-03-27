b2bc7f27762ac45acc298f67b0b969d65a5658b7
dfldiego <dfl.diego92@gmail.com>
2021-03-26 23:58:50 -0300

    leer argumentos por URL. obtener id en detalle-heroe. redireccionar si URL no existe.

--- src/components/DetalleHeroe.jsx
+++ src/components/DetalleHeroe.jsx
@@ -1,6 +1,21 @@
 import React from 'react'
+import { Redirect, useParams } from 'react-router'
+import getHeroById from '../selectors/getHeroById';
 
 const DetalleHeroe = () => {
+
+    // obtengo el id del heroe seleccionado
+    const { id } = useParams();
+    console.log(id);
+    const heroe = getHeroById(id);
+    console.log(heroe);
+
+    if (!heroe) {
+        return <Redirect to="/" />;
+    }
+
+    const { nombre, poderes, bio, img, aparicion, casa } = heroe;
+
     return (
         <div>
             <h1>Detalle Heroe works!</h1>
